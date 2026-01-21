15d77b1322aef72e1a1d9cdc39389e2471be2d19
byoung-uk213 <hhtt2503@gmail.com>
2026-01-21 16:06:03 +0900

    환경 설정 완료

---
+++ src/main/java/com/example/cellphoneback/CellPhoneBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cellphoneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellPhoneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CellPhoneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cellphoneback/CellPhoneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cellphoneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellPhoneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
