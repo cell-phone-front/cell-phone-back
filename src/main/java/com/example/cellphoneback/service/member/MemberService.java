3f2e2de093c8892dfc14d7c0032faa3ec8f3c90a
miseon768-tech <miseon768@gmail.com>
2026-01-22 16:06:12 +0900

    member controller, service 세팅

--- src/main/java/com/example/cellphoneback/service/member/MemberService.java
+++ src/main/java/com/example/cellphoneback/service/member/MemberService.java
@@ -1,5 +1,34 @@
 
 package com.example.cellphoneback.service.member;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+
+
+//    POST	/api/member/login	로그인	all
+
+
+//    POST	/api/member/create	계정 생성	admin
+
+
+//    POST	/api/member/parse/xls	멤버 엑셀 파싱	admin
+
+
+//    GET	/api/member	전체 멤버 조회	admin
+
+
+//    PUT	/api/member	멤버 정보 수정	admin
+
+
+//    DELETE	/api/member	멤버 정보 삭제	admin
+
+
+//    GET	/api/member/{memberId}	계정 조회	본인
+
+
 }
