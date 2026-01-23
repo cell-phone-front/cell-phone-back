d6b94e0d205c6c2248d7acc899d1f9a8d39573fb
miseon768-tech <miseon768@gmail.com>
2026-01-23 18:11:27 +0900

    notice, community, comment : controller, service 어노테이션 추가

--- src/main/java/com/example/cellphoneback/controller/community/CommunityController.java
+++ src/main/java/com/example/cellphoneback/controller/community/CommunityController.java
@@ -1,4 +1,20 @@
 package com.example.cellphoneback.controller.community;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@SecurityRequirement(name = "bearerAuth")
+//@Tag(name = "Comment", description = "댓글 관련 API")
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("/api/community")
 public class CommunityController {
+
+//    community	POST	/api/community	게시글 작성	planner, worker
+//    community	PUT	/api/community	게시글 수정	planner, worker
+//    community	DELETE	/api/community	게시글 삭제	planner, worker
+//    community	GET	/api/community	게시글 조회	all
 }
--- src/main/java/com/example/cellphoneback/service/notice/NoticeService.java
+++ src/main/java/com/example/cellphoneback/service/notice/NoticeService.java
@@ -1,5 +1,15 @@
 
 package com.example.cellphoneback.service.notice;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class NoticeService {
+
+    //    notice	POST	/api/notice	공지사항 작성	admin, planner
+//    notice	PUT	/api/notice	공지사항 수정	admin, planner
+//    notice	DELETE	/api/notice	공지사항 삭제	admin, planner
+//    notice	GET	/api/notice	공지사항 조회	all
 }
