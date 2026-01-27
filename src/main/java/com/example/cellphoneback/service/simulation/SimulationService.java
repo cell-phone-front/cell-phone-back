f8ad0d6bb68949af4c3086192561aed8539784dc
miseon768-tech <miseon768@gmail.com>
2026-01-27 11:11:04 +0900

    operation, service 어노테이션 추가

--- src/main/java/com/example/cellphoneback/service/simulation/SimulationService.java
+++ src/main/java/com/example/cellphoneback/service/simulation/SimulationService.java
@@ -1,5 +1,10 @@
 
 package com.example.cellphoneback.service.simulation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class SimulationService {
 }
