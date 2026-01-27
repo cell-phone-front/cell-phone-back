2bf8af704f51d9554f29250fc6de200a791e9ebb
miseon768-tech <miseon768@gmail.com>
2026-01-27 11:18:58 +0900

    operation, controller service 파일 추가

---
+++ src/main/java/com/example/cellphoneback/controller/operation/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.cellphoneback.controller.operation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@SecurityRequirement(name = "bearerAuth")
+//@Tag(name = "Comment", description = "댓글 관련 API")
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("/api/operation")
+public class ProductController {
+}
---
+++ src/main/java/com/example/cellphoneback/service/operation/ProductService.java
@@ -0,0 +1,10 @@
+
+package com.example.cellphoneback.service.operation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+}
