2765f880918cd0894c77aadeabb276cefd3c5569
miseon768-tech <miseon768@gmail.com>
2026-02-03 17:17:51 +0900

    전체검색에 pin 조회 추가

--- src/main/java/com/example/cellphoneback/dto/response/notice/SearchNoticeByIdResponse.java
+++ src/main/java/com/example/cellphoneback/dto/response/notice/SearchNoticeByIdResponse.java
@@ -13,12 +13,14 @@ public class SearchNoticeByIdResponse {
     String title;
     String content;
     LocalDateTime createdAt;
+    boolean pinned;
 
     public static SearchNoticeByIdResponse fromEntity(Notice notice){
         return SearchNoticeByIdResponse.builder()
                 .title(notice.getTitle())
                 .content(notice.getContent())
                 .createdAt(notice.getCreatedAt())
+                .pinned(notice.isPinned())
                 .build();
     }
 }
